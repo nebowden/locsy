86fabf0eb7194dc153af54424812edc0dc2cce96
nebowden <nathanebowden@gmail.com>
2015-01-21 08:06:22 -0600

    basic html and css complete

---
+++ js/app.js
@@ -0,0 +1,16 @@
+$(document).ready( function() {
+var estyKey = "lw0y75472kxputmk49tlocx6";
+/*
+get request to LOC using user search term
+parse LOC data to get image and subject/photo information 
+display images from LOC
+
+user clicks on image
+get data from image
+get request to Etsy using LOC image subject
+display image, name, shop & price from Etsy
+link through Etsy images to Etsy site
+new search clears prior results
+*/
+
+}
