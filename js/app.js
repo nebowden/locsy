7eaf06d061421407aa952b4ea710c6b4847436bf
nebowden <nathanebowden@gmail.com>
2015-01-22 08:12:37 -0600

    added js for call to LOC

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $(document).ready( function() {
-var estyKey = "lw0y75472kxputmk49tlocx6";
+var etsyKey = "lw0y75472kxputmk49tlocx6";
 /*
 get request to LOC using user search term
 parse LOC data to get image and subject/photo information 
@@ -13,4 +13,43 @@ link through Etsy images to Etsy site
 new search clears prior results
 */
 
-}
+var showLocImages = function(locImage) {
+
+	var resultLoc = $('.templates .result-loc').clone();
+
+	var imageElem = resultLoc.find('.loc-image');
+	imageElem.attr('src', locImage.image.square);
+
+	return resultLoc;
+};
+
+var getLocImages = function(keyword) {
+	var locParams = {
+		q: keyword,
+		fo: 'json'
+	};
+
+	var result = $.ajax({
+		url: "http://loc.gov/pictures/search",
+		data: locParams,
+		type: "get",
+		dataType: "jsonp"
+	})
+	.done(function(result){
+		console.log(result.results);
+		$.each(result.results, function(index, photo){
+			var locImage = showLocImages(photo);
+			$(".loc-image-subcontainer").append(locImage);
+			/*console.log(photo.subjects);*/
+		});
+	});
+};
+
+$("#user-search-form").submit(function(event) {
+		var keyword = $("#search-field").val();
+		event.preventDefault();
+		getLocImages(keyword);
+	});
+
+
+});
